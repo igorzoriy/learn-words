0ba50dbb726309fb6f9099afdd99e92c22324d20
Igor Zoriy <gh@igorzoriy.com>
2016-02-16 15:58:18 +0100

    Add redux logger

--- src/store.js
+++ src/store.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware } from 'redux'
 import { browserHistory } from 'react-router'
 import { syncHistory } from 'react-router-redux'
+import createLogger from 'redux-logger'
 import reducer from './reducer'
 
 export default function (initialState = {}) {
     const middlewares = [
         syncHistory(browserHistory),
+        createLogger({
+            collapsed: true,
+        }),
     ]
 
     const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)

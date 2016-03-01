a492f3002231a8d08c86cdb564242a835b0ef7fa
Igor Zoriy <gh@igorzoriy.com>
2016-03-01 18:23:43 +0100

    Add static template for new vocabulary item

--- src/layout/App.js
+++ src/layout/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
+import AppBar from 'material-ui/lib/app-bar'
 
 export default class App extends Component {
     render () {
         return (
             <div>
-                <header className="header">
-                    Header
-                </header>
+                <AppBar title="Learn Words" />
                 <main className="content">
                     { this.props.children }
                 </main>
--- src/vocabulary/NewItemPage.js
+++ src/vocabulary/NewItemPage.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import TextField from 'material-ui/lib/text-field'
+import RaisedButton from 'material-ui/lib/raised-button'
 
 export default class NewItemPage extends Component {
     render () {
         return (
             <div>
-                new item page
+                <TextField hintText="Word" />
+                <TextField hintText="Translation" />
+                <br />
+                <RaisedButton label="Add" primary={ true } />
             </div>
         )
     }

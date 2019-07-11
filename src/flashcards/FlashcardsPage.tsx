17e31de82a17a2dda61d5a0eb0c1377c5e888943
Igor Zoriy <me@igorzoriy.com>
2019-07-12 00:35:05 +0200

    Refactor FlashcardsPage component

--- src/flashcards/FlashcardsPage.tsx
+++ src/flashcards/FlashcardsPage.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { FunctionComponent, useEffect } from "react"
 import { connect } from "react-redux"
 import { Dispatch } from "redux"
 import { ICard, IStoreState, Statuses } from "../types"
@@ -16,83 +16,56 @@ import {
 import { Flashcard } from "./Flashcard"
 
 interface IProps {
-    initFlashcards: () => void
-    flipCurrentFlashcard: () => void
-    prevFlashcard: () => void
-    nextFlashcard: () => void
+    init: () => void
+    flip: () => void
+    prev: () => void
+    next: () => void
     status: Statuses
     isEmpty: boolean
     showFront: boolean
     card: ICard
 }
 
-export class FlashcardsPage extends React.PureComponent<IProps> {
-    constructor(props: IProps) {
-        super(props)
-    }
+export const FlashcardsPage: FunctionComponent<IProps> = ({
+    init,
+    flip,
+    prev,
+    next,
+    status,
+    isEmpty,
+    showFront,
+    card,
+}) => {
+    useEffect(() => {
+        init()
+    }, [])
 
-    public componentDidMount() {
-        this.props.initFlashcards()
-    }
+    const content = isEmpty ? <EmptyList key="empty" /> : <Flashcard
+        {...card}
+        showFront={showFront}
+        onTap={flip}
+        onSwipeLeft={next}
+        onSwipeRight={prev}
+        key="card"
+    />
 
-    private handleTap = () => {
-        this.props.flipCurrentFlashcard()
-    }
-
-    private handleSwipeLeft = () => {
-        this.props.nextFlashcard()
-    }
-
-    private handleSwipeRight = () => {
-        this.props.prevFlashcard()
-    }
-
-    private renderCardContent(card: ICard, showFront: boolean) {
-        return <Flashcard
-            {...card}
-            showFront={showFront}
-            onTap={this.handleTap}
-            onSwipeLeft={this.handleSwipeLeft}
-            onSwipeRight={this.handleSwipeRight}
-            key="card"
-        />
-    }
-
-    public render() {
-        const { status, isEmpty, card, showFront } = this.props
-        const content = []
-
-        switch (status) {
-            case Statuses.Init:
-            case Statuses.Request:
-                content.push(<Preloader key="preloader" />)
-                break
-            case Statuses.Success:
-                if (isEmpty) {
-                    content.push(<EmptyList key="empty" />)
-                } else {
-                    content.push(this.renderCardContent(card, showFront))
-                }
-                break
-            case Statuses.Failure:
-                content.push(<Alert key="error" type="danger" message="Failed to load vocabulary items." />)
-                break
-        }
-
-        return (
-            <div>
-                <PageTitle title="Flashcards" />
-                {content}
-            </div>
-        )
-    }
+    return (
+        <div>
+            <PageTitle title="Flashcards" />
+            {status === Statuses.Init && <Preloader key="preloader" />}
+            {status === Statuses.Request && <Preloader key="preloader" />}
+            {status === Statuses.Failure &&
+                <Alert key="error" type="danger" message="Failed to load vocabulary items." />}
+            {status === Statuses.Success && content}
+        </div>
+    )
 }
 
-const mapStateToProps = (state: IStoreState) => {
+const mapStateToProps = (state: IStoreState): Partial<IProps> => {
     const { status, hash } = state.vocabulary.entities
     const { ids, currentIndex, showFront } = state.flashcards
 
-    let card = {}
+    let card: ICard
     const id = ids[currentIndex]
     if (hash[id]) {
         card = {
@@ -109,11 +82,11 @@ const mapStateToProps = (state: IStoreState) => {
     }
 }
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-    initFlashcards: () => dispatch(initFlashcards()),
-    flipCurrentFlashcard: () => dispatch(flipCurrentFlashcard()),
-    prevFlashcard: () => dispatch(prevFlashcard()),
-    nextFlashcard: () => dispatch(nextFlashcard()),
+const mapDispatchToProps = (dispatch: Dispatch): Partial<IProps> => ({
+    init: () => dispatch(initFlashcards()),
+    flip: () => dispatch(flipCurrentFlashcard()),
+    prev: () => dispatch(prevFlashcard()),
+    next: () => dispatch(nextFlashcard()),
 })
 
 export const FlashcardsPageContainer = connect(mapStateToProps, mapDispatchToProps)(FlashcardsPage)

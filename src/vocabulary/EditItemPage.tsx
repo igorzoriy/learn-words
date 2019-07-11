b0f0d9328b3cf696f2f2a0af8a011207686f0f0e
Igor Zoriy <me@igorzoriy.com>
2019-07-11 23:49:41 +0200

    Refactor EditItemPage component

--- src/vocabulary/EditItemPage.tsx
+++ src/vocabulary/EditItemPage.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { FunctionComponent, useEffect, useRef } from "react"
 import { connect } from "react-redux"
 import { match } from "react-router"
 import { Dispatch } from "redux"
@@ -13,9 +13,9 @@ import {
 import { EditForm } from "./EditForm"
 
 export interface IProps {
-    fillVocabularyForm: (id: string) => void
-    updateVocabularyEditForm: (phrase: string, translation: string) => void
-    editVocabularyItem: (card: ICard) => void,
+    fillForm: (id: string) => void
+    updateForm: (phrase: string, translation: string) => void
+    editItem: (card: ICard) => void
     match: match<{ id: string }>
     status: Statuses
     phrase: string
@@ -29,73 +29,68 @@ enum Fields {
     translation = "translation",
 }
 
-export class EditItemPage extends React.PureComponent<IProps> {
-    private phraseRef: React.RefObject<HTMLInputElement>
-
-    constructor(props: IProps) {
-        super(props)
-        this.phraseRef = React.createRef()
-    }
-
-    public componentDidMount() {
-        this.props.fillVocabularyForm(this.props.match.params.id)
-    }
-
-    public componentDidUpdate(prevProps: IProps) {
-        if (prevProps.status === Statuses.Request && this.props.status === Statuses.Success) {
-            this.phraseRef.current.focus()
+export const EditItemPage: FunctionComponent<IProps> = ({
+    status,
+    phrase,
+    translation,
+    errorMessage,
+    successMessage,
+    match: { params: { id } },
+    fillForm,
+    updateForm,
+    editItem,
+}) => {
+    const phraseRef = useRef(null)
+    useEffect(() => {
+        fillForm(id)
+        phraseRef.current.focus()
+    }, [])
+    useEffect(() => {
+        if (status === Statuses.Success) {
+            phraseRef.current.focus()
         }
-    }
+    }, [status])
 
-    private handleFormChange = (type: Fields, e: React.ChangeEvent<HTMLInputElement>) => {
-        const { phrase, translation } = this.props
+    const handleFormChange = (type: Fields, e: React.ChangeEvent<HTMLInputElement>) => {
         if (type === Fields.phrase) {
-            this.props.updateVocabularyEditForm(e.target.value, translation)
+            updateForm(e.target.value, translation)
         } else {
-            this.props.updateVocabularyEditForm(phrase, e.target.value)
+            updateForm(phrase, e.target.value)
         }
     }
-
-    private handleFormSubmit = (e: React.FormEvent) => {
+    const handleFormSubmit = (e: React.FormEvent) => {
         e.preventDefault()
-
-        const { phrase, translation, match: { params: { id } } } = this.props
-        this.props.editVocabularyItem({
+        editItem({
             id,
             phrase,
             translation,
         })
     }
 
-    public render() {
-        const { status, phrase, translation, errorMessage, successMessage } = this.props
-
-        return (
-            <div>
-                <PageTitle title="Edit phrase" />
-                <EditForm
-                    phraseRef={this.phraseRef}
-                    onPhraseChange={this.handleFormChange.bind(this, Fields.phrase)}
-                    onTranslationChange={this.handleFormChange.bind(this, Fields.translation)}
-                    onSubmit={this.handleFormSubmit}
-                    inProgress={status === Statuses.Request}
-                    phrase={phrase}
-                    translation={translation}
-                />
-                {errorMessage.length > 0 && <Alert key="error" type="danger" message={errorMessage} />}
-                {successMessage.length > 0 && <Alert key="success" type="success" message={successMessage} />}
-            </div>
-        )
-    }
+    return (
+        <div>
+            <PageTitle title="Edit phrase" />
+            <EditForm
+                phraseRef={phraseRef}
+                onPhraseChange={handleFormChange.bind(this, Fields.phrase)}
+                onTranslationChange={handleFormChange.bind(this, Fields.translation)}
+                onSubmit={handleFormSubmit}
+                inProgress={status === Statuses.Request}
+                phrase={phrase}
+                translation={translation}
+            />
+            {errorMessage.length > 0 && <Alert key="error" type="danger" message={errorMessage} />}
+            {successMessage.length > 0 && <Alert key="success" type="success" message={successMessage} />}
+        </div>
+    )
 }
 
 const mapStateToProps = ({ vocabulary: { edit } }: IStoreState) => ({...edit})
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-    fillVocabularyForm: (id: string) => dispatch(fillVocabularyForm(id)),
-    updateVocabularyEditForm: (phrase: string, translation: string) =>
-        dispatch(updateVocabularyEditForm(phrase, translation)),
-    editVocabularyItem: (card: ICard) => dispatch(editVocabularyItem(card)),
+    fillForm: (id: string) => dispatch(fillVocabularyForm(id)),
+    updateForm: (phrase: string, translation: string) => dispatch(updateVocabularyEditForm(phrase, translation)),
+    editItem: (card: ICard) => dispatch(editVocabularyItem(card)),
 })
 
 export const EditItemPageContainer = connect(mapStateToProps, mapDispatchToProps)(EditItemPage)
